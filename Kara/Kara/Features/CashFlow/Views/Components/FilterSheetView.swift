7cb21c0791e7a8396861f341aa4fc8d009e7367b
ardodillo <samuelbonardo99@gmail.com>
2026-08-24 07:20:21 +0700

    feat(cashflow): add filter sheet component

---
+++ Kara/Kara/Features/CashFlow/Views/Components/FilterSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  FilterSheetView.swift
+//  
+//
+//  Created by Samuel Bonardo on 23/08/26.
+//
+
+import SwiftUI
+
+struct FilterSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterSheetView()
+}
