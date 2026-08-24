297f45733deadfa949b27898aef35beff419f660
ardodillo <samuelbonardo99@gmail.com>
2026-08-24 17:00:17 +0700

    feat(cashflow): add search bar, active filter button, and (dummy) date

---
+++ Kara/Kara/Features/CashFlow/Views/Components/SearchBarFilterButton.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBarFilterButton.swift
+//  Kara
+//
+//  Created by Samuel Bonardo on 24/08/26.
+//
+
+import SwiftUI
+
+struct SearchBarFilterButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBarFilterButton()
+}
