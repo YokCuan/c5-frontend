ddf825cea57bb2c35223d53a255e5b77fa24b88d
ardodillo <samuelbonardo99@gmail.com>
2026-08-25 18:04:52 +0700

    feat Catat Pembayaran body

---
+++ Kara/Kara/Features/CashFlow/Views/Components/SheetCatatPembayaran.swift
@@ -0,0 +1,18 @@
+//
+//  SheetCatatPembayaran.swift
+//  Kara
+//
+//  Created by Samuel Bonardo on 25/08/26.
+//
+
+import SwiftUI
+
+struct SheetCatatPembayaran: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetCatatPembayaran()
+}
