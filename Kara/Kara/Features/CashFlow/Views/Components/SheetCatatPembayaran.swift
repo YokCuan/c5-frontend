ff46c9bff7453b7555c5a865efbf459b51f2212f
ardodillo <samuelbonardo99@gmail.com>
2026-08-25 18:05:21 +0700

    feat: buat body catat pembayaran

--- Kara/Kara/Features/CashFlow/Views/Components/SheetCatatPembayaran.swift
+++ Kara/Kara/Features/CashFlow/Views/Components/SheetCatatPembayaran.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct SheetCatatPembayaran: View {
+    @State private var bayar: String = "Masukkan nominal pembayaran"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Catat Pembayaran")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.primary)
+                .frame(maxWidth: .infinity, alignment: .init(horizontal: .leading, vertical: .top))
+            
+            HStack {
+                Text("Bu Sherin")
+                Text("· Sisa")
+                Text("Rp5.000")
+            }
+            .font(.callout)
+            .fontWeight(.regular)
+            .foregroundStyle(.secondary)
+            .frame(maxWidth: .infinity, alignment: .init(horizontal: .leading, vertical: .top))
+            
+            Divider()
+            
+            Text("Jumlah Pembayaran")
+                .font(.callout)
+                .fontWeight(.regular)
+                .foregroundStyle(.secondary)
+                .frame(maxWidth: .infinity, alignment: .init(horizontal: .leading, vertical: .top))
+            
+            HStack {
+                Text("Rp")
+                    .font(.callout)
+                    .fontWeight(.regular)
+                    .foregroundStyle(.secondary)
+            TextField("Cari transaksi...", text: $bayar)
+                .foregroundColor(Color.primary)
+                .font(.title2)
+                .fontWeight(.bold)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .init(horizontal: .leading, vertical: .top))
+            .background(Color.secondary.opacity(0.1))
+        }
+        .padding()
     }
 }
 
