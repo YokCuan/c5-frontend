1368efddea35d538679f01ad1efd73ad7de2869b
ardodillo <samuelbonardo99@gmail.com>
2026-09-07 12:27:49 +0700

    add: some code to make automatic dismiss keyboard

--- Kara/Kara/ContentView.swift
+++ Kara/Kara/ContentView.swift
@@ -3,5 +3,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         MainTabView()
+            .contentShape(Rectangle())
+            .onTapGesture {
+                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+            }
     }
 }
